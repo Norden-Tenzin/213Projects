76df489f57f9f1fa822d6ed1fb64d82999fc2a7e
Vedant <vm.WorkAddress@gmail.com>
2021-01-30 20:01:35 -0500

    added Date

--- Project1/src/Book.java
+++ Project1/src/Book.java
@@ -2,45 +2,45 @@ package Project1.src;
 
 public class Book {
 
-  private String number; //a 5-digit serial number unique to the book
-  private String name;
-  private boolean checkedOut;
-  private Date datePublished;
+	private String number; //a 5-digit serial number unique to the book
+  	private String name;
+	private boolean checkedOut;
+	private Date datePublished;
 
-  public Book(String number, String name, Date datePublished) {
-    // this.number = number;
-    this.name = name;
-    this.checkedOut = false;
-    this.datePublished = datePublished;
-  }
+	public Book(String number, String name, Date datePublished) {
+		// this.number = number;
+		this.name = name;
+		this.checkedOut = false;
+		this.datePublished = datePublished;
+	}
 
-  public String isAvailable(){
-    if(this.checkedOut){
-      return "is not available";
-    } else return "is available";
-  }
+	public String isAvailable(){
+		if(this.checkedOut){
+			return "is not available";
+		} else return "is available";
+	}
 
-  public void setNumber(String number) {
-    this.number = number;
-  }
+	public void setNumber(String number) {
+		this.number = number;
+	}
 
-  public String getNumber() {
-    return this.number;
-  }
+	public String getNumber() {
+		return this.number;
+	}
 
-  public void setCheckout(boolean checkedOut) {
-    this.checkedOut = checkedOut;
-  }
+	public void setCheckout(boolean checkedOut) {
+		this.checkedOut = checkedOut;
+	}
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this.number.equals(((Book)obj).number)){
-      return true;
-    } else return false;
-  }
+	@Override
+	public boolean equals(Object obj) {
+		if (this.number.equals(((Book)obj).number)){
+			return true;
+		} else return false;
+	}
 
-  @Override
-  public String toString() {
-    return ("Book#"+number+"::"+name+"::"+datePublished.toString()+"::"+isAvailable());
-  }
+	@Override
+	public String toString() {
+		return ("Book#"+number+"::"+name+"::"+datePublished.toString()+"::"+isAvailable());
+	}
 }
--- Project1/src/Date.java
+++ Project1/src/Date.java
@@ -1,14 +1,53 @@
-package Project1.src;
+// package Project1.src;
+import java.util.Calendar;
 
 public class Date {
 
-  private int year;
-  private int month;
-  private int day;
+	private int year;
+	private int month;
+	private int day;
 
-  public Date(String date) {} //taking mm/dd/yyyy and create a Date object
+	public Date() {
+		Calendar dateCal = Calendar.getInstance();
+		this.year = dateCal.get(Calendar.YEAR);
+		this.month = dateCal.get(Calendar.MONTH) + 1;
+		this.day = dateCal.get(Calendar.DATE);
+	}
 
-  public Date() {} //return today’s date
+	public Date(String date) {
+		String dateParts [] = date.split("/");
+		this.month = Integer.parseInt(dateParts[0]);
+		this.day = Integer.parseInt(dateParts[1]);
+		this.year = Integer.parseInt(dateParts[2]);
+	}
 
-  public isValid() {}
+
+	
+	public boolean isValid() {
+		
+		int CURRENT_YEAR = 2021;
+
+		// Year validation
+		if (this.year < 1900 || this.year > CURRENT_YEAR || this.month > 12) 
+				return false;
+		
+		// Checks to see if the year is a leap year.
+		int FEB = 28;
+		if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) 
+				FEB = 29; 
+
+		// if the current day exceeds the maximum day, return false
+		int [] maxDaysPerMonth = {31, FEB, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		if (this.day >= maxDaysPerMonth(this.year)[this.month-1]) 
+				return false;
+		
+		return true;
+	}
+
+	public static void main(String[]cheese){
+		Date b1 = new Date("8/1/1999");
+		System.out.println(b1.day);
+		System.out.println(b1.isValid());
+	}
+
 }
