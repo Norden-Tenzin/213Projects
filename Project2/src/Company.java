86c1d1e1c78b96229259973edfd8cb4db5c9776a
Norden-Tenzin <nordentenzin29@gmail.com>
2021-02-22 14:18:23 -0500

    Payroll

--- Project2/src/Company.java
+++ Project2/src/Company.java
@@ -13,119 +13,117 @@ package Project2.src;
 
 public class Company {
 
-   private Employee[] employees;
-   private int numEmployee;
-   final int STARTING_LIBRARY_SIZE = 4;
+  private Employee[] employees;
+  private int numEmployee;
+  final int STARTING_LIBRARY_SIZE = 4;
 
-   /**
-    * Default constructor. Sets default values.
-    *
-    */
-   public Company() {
+  /**
+   * Default constructor. Sets default values.
+   *
+   */
+  public Company() {
     employees = new Employee[STARTING_LIBRARY_SIZE];
-        numEmployee = 0;
-   }
+    numEmployee = 0;
+  }
 
-   /**
-    * Looks through the books array to find the book with the same number.
-    *
-    * @param book book Object
-    * @return index of the book if book is found in the books array. otherwise -1.
-    */
-   private int find(Employee employee) {
-      for (int i = 0; i < numEmployee; i++) {
-         if (employees[i].equals(employee))
-            return i;
+  /**
+   * Looks through the books array to find the book with the same number.
+   *
+   * @param book book Object
+   * @return index of the book if book is found in the books array. otherwise -1.
+   */
+  private int find(Employee employee) {
+    for (int i = 0; i < numEmployee; i++) {
+      if (employees[i].equals(employee)) return i;
+    }
+    return -1;
+  }
+
+  /**
+   * When called increases the size of the books array by STARTING_LIBRARY_SIZE
+   */
+  private void grow() {
+    // Double the length of the array
+    Employee[] newArray = new Employee[employees.length +
+    STARTING_LIBRARY_SIZE];
+
+    // Move elements into new array.
+    for (int i = 0; i < numEmployee; i++) {
+      newArray[i] = employees[i];
+    }
+
+    // Set books to new array
+    employees = newArray;
+  }
+
+  /**
+   * Adds book object into the books array
+   *
+   * @param book book Object
+   */
+  public void add(Employee employee) {
+    // if the library is full, we double the size.
+    if (employees[employees.length - 1] != null) grow();
+
+    // Go through the array, add a book at the first empty spot.
+    for (int i = 0; i < employees.length; i++) {
+      if (employees[i] == null) {
+        // once we find a null position, we set the index's value to the book and stop
+        // the loop.
+        employees[i] = employee;
+        break;
       }
-      return -1;
-   }
+    }
+    numEmployee++;
+  }
 
-   /**
-    * When called increases the size of the books array by STARTING_LIBRARY_SIZE
-    */
-   private void grow() {
-      // Double the length of the array
-      Employee[] newArray = new Employee[employees.length + STARTING_LIBRARY_SIZE];
-
-      // Move elements into new array.
-      for (int i = 0; i < numEmployee; i++) {
-         newArray[i] = employees[i];
+  public boolean setHours(Employee employee) {
+    int indexEmployee = find(employee);
+    if (employees[indexEmployee] instanceof Parttime) {
+      if (employee.getHours() >= 0) {
+        employees[indexEmployee] = employee;
       }
+    }
+  }
 
-      // Set books to new array
-      employees = newArray;
-   }
+  /**
+   * Removes the given book object from the books array
+   *
+   * @param book book Object
+   * @return true if books is removed, false otherwise.
+   */
+  public boolean remove(Employee employee) {
+    int indexEmployee = find(employee);
 
-   /**
-    * Adds book object into the books array
-    * 
-    * @param book book Object
-    */
-   public void add(Employee employee) {
-      // if the library is full, we double the size.
-      if (employees[employees.length - 1] != null)
-         grow();
+    // If the book does not exist.
+    if (indexEmployee == -1) return false;
 
-      // Go through the array, add a book at the first empty spot.
-      for (int i = 0; i < employees.length; i++) {
-         if (employees[i] == null) {
-            // once we find a null position, we set the index's value to the book and stop
-            // the loop.
-            employees[i] = employee;
-            break;
-         }
-      }
-      numEmployee++;
-   }
+    // shift all values in the library by one to the left.
+    for (int i = indexEmployee + 1; i < employees.length; i++) {
+      // Move current book back.
+      employees[i - 1] = employees[i];
+    }
 
-   public boolean setHours(Employee employee) {
-      int indexEmployee = find(employee);
-      if(employees[indexEmployee] instanceof Parttime){
-         if(employee.getHours() >= 0){
-            employees[indexEmployee] = employee;
-         }
-      }
-   }
+    // since we removed, we need to decrease by 1.
+    numEmployee--;
+    return true;
+  }
 
-   /**
-    * Removes the given book object from the books array
-    *
-    * @param book book Object
-    * @return true if books is removed, false otherwise.
-    */
-   public boolean remove(Employee employee) {
-      int indexEmployee = find(employee);
+  /**
+   * Prints the books array
+   */
+  public void print() {
+    for (int i = 0; i < numEmployee; i++) {
+      System.out.println(employees[i].toString());
+    }
+  }
 
-      // If the book does not exist.
-      if (indexEmployee == -1)
-         return false;
+  /**
+   * Sorts the books array by Dates in ascending order, then prints the books
+   * array
+   */
 
-      // shift all values in the library by one to the left.
-      for (int i = indexEmployee + 1; i < employees.length; i++) {
-         // Move current book back.
-         employees[i - 1] = employees[i];
-      }
-
-      // since we removed, we need to decrease by 1.
-      numEmployee--;
-      return true;
-   }
-
-   /**
-    * Prints the books array
-    */
-   public void print() {
-      for (int i = 0; i < numEmployee; i++) {
-         System.out.println(employees[i].toString());
-      }
-   }
-
-   /**
-    * Sorts the books array by Dates in ascending order, then prints the books
-    * array
-    */
-
-    /*
+  /*
    public void printByDate() {
       Employee temp;
       for (int i = 0; i <= this.books.length - 1; i++) {
@@ -165,12 +163,12 @@ public class Company {
    } // print the list of books by datePublished (ascending)
    */
 
-   /**
-    * Sorts the books array by Book number in assending order, then prints the
-    * books array.
-    */
+  /**
+   * Sorts the books array by Book number in assending order, then prints the
+   * books array.
+   */
 
-    /*
+  /*
    public void printByNumber() {
       Book temp;
       for (int i = 0; i <= this.books.length - 1; i++) {
@@ -190,12 +188,12 @@ public class Company {
    } // print the list of books by number (ascending)
    */
 
-   /**
-    * Helper method to get the number of books.
-    * 
-    * @return the number of books in the books array
-    */
-   public int getNumEmployee() {
-      return numEmployee;
-   }
+  /**
+   * Helper method to get the number of books.
+   *
+   * @return the number of books in the books array
+   */
+  public int getNumEmployee() {
+    return numEmployee;
+  }
 }
--- Project2/src/PayrollProcessing.java
+++ Project2/src/PayrollProcessing.java
@@ -3,8 +3,157 @@ package Project2.src;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+import org.graalvm.compiler.nodes.calc.IntegerDivRemNode;
+
 public class PayrollProcessing {
-  public void run(){
-     
+
+  public void run() {
+    Company company = new Company();
+    Scanner sc = new Scanner(System.in);
+    // Default message when starting run.
+    System.out.println("Library Kiosk running.");
+
+    // Keep running as long as user enters line
+    while (sc.hasNext()) {
+      // Tokenize the string and split by ,
+      StringTokenizer input = new StringTokenizer(sc.nextLine(), ",");
+
+      String command = "";
+      String name = "";
+      String department = "";
+      String date = "";
+      String pay = "";
+      String role = "";
+
+      // Check the input three times and set each command accordingly.
+      if (input.hasMoreTokens()) command = input.nextToken(); // command is the first token value
+      if (input.hasMoreTokens()) name = input.nextToken(); // bookKey can be either book number or name
+      if (input.hasMoreTokens()) department = input.nextToken(); // Date will be the third token value
+      if (input.hasMoreTokens()) date = input.nextToken();
+      if (input.hasMoreTokens()) pay = input.nextToken();
+      if (input.hasMoreTokens()) role = input.nextToken();
+
+      // helper boolean to check if there is only one argument
+      boolean onlyOneArgument = name.equals("") && department.equals("");
+
+      // Base case: If Q, quit the program
+      if (command.equals("Q")) {
+        break;
+      }
+
+      // If there is only one argument passed ie "PA", "PD", "PN"
+      if (onlyOneArgument) {
+        switch (command) {
+          case "PA":
+            if (company.getNumEmployee() == 0) {
+              System.out.println("Library catalog is empty!");
+            } else {
+              System.out.println("**List of books in the library.");
+              company.print();
+              System.out.println("**End of list");
+            }
+            break;
+          case "PD":
+            if (company.getNumEmployee() == 0) {
+              System.out.println("Bookshelf is empty!");
+            } else {
+              System.out.println("**List of books by date published.");
+              company.getNumEmployee();
+              System.out.println("**End of list");
+            }
+            break;
+          case "PN":
+            if (company.getNumEmployee() == 0) {
+              System.out.println("Bookshelf is empty!");
+            } else {
+              System.out.println("**List of books by the book numbers.");
+              company.getNumEmployee();
+              System.out.println("**End of list");
+            }
+            break;
+          default:
+            System.out.println("Invalid command!");
+        }
+      } else {
+        // If there is more than one command ie A, R, O, I commands.
+        switch (command) {
+          case "AP":
+            if (new Date(date).isValid()) {
+              // Add the new book to the library
+              Parttime employee = new Parttime(
+                new Profile(name, department, new Date(date)),
+                Double.parseDouble(pay)
+              );
+              company.add(employee);
+              System.out.println("Employee added.");
+            } else {
+              System.out.println("Invalid Date!");
+              continue;
+            }
+            break;
+          case "AF":
+            if (new Date(date).isValid()) {
+              // Add the new book to the library
+              Fulltime employee = new Fulltime(
+                new Profile(name, department, new Date(date)),
+                Double.parseDouble(pay)
+              );
+              company.add(employee);
+              System.out.println("Employee added.");
+            } else {
+              System.out.println("Invalid Date!");
+              continue;
+            }
+            break;
+          case "AM":
+            if (new Date(date).isValid()) {
+              // Add the new book to the library
+              Fulltime employee = new Fulltime(
+                new Profile(name, department, new Date(date)),
+                Double.parseDouble(pay), Integer.parseInt(role)
+              );
+              company.add(employee);
+              System.out.println("Employee added.");
+            } else {
+              System.out.println("Invalid Date!");
+              continue;
+            }
+            break;
+          case "R":
+            Book removeBook = new Book(bookKey);
+            boolean wasRemoved = library.remove(removeBook);
+            // check to see if the book was removed
+            if (wasRemoved) System.out.println(
+              "Book# " + bookKey + " removed"
+            ); else System.out.println(
+              "Unable to remove, the library does not have this book."
+            );
+            break;
+          case "O":
+            Book checkOutBook = new Book(bookKey);
+            // library.checkOut returns true if the book was able to be checked out.
+            boolean checkOut = library.checkOut(checkOutBook);
+            if (checkOut) System.out.println(
+              "You've checked out Book#" + bookKey + ". Enjoy!"
+            ); else System.out.println(
+              "Book#" + bookKey + " is not available."
+            );
+            break;
+          case "I":
+            Book returnsBook = new Book(bookKey);
+            // library.returns returns true if the book was able to be returned.
+            boolean returns = library.returns(returnsBook);
+            if (returns) System.out.println(
+              "Book#" + bookKey + " return has completed. Thanks!"
+            ); else System.out.println(
+              "Unable to return Book#" + bookKey + "."
+            );
+            break;
+          default:
+            System.out.println("Invalid command!");
+        }
+      }
+    }
+    System.out.println("Kiosk session ended.");
   }
 }
