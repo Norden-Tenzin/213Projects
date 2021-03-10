f5da1027a04d66c21738edd3c9f45de6cd2d154b
Vedant <vm.WorkAddress@gmail.com>
2021-03-10 17:42:51 -0500

    not working, but for example

--- Project3/src/HelloWorld.java
+++ Project3/src/HelloWorld.java
@@ -1,34 +1,28 @@
 package Project3.src;
- 
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
- 
-public class HelloWorld extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World HUGA BUGA");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
+
+public  class HelloWorld extends Application {
+    @Override 
+   public  void start (Stage primaryStage) {
+        try {
+           FXMLLoader fxmlLoader = new FXMLLoader (getClass (). getResource ( "TestUI.fxml" ));
+           Parent root = (Parent) fxmlLoader.load ();
+           Scene scene = new Scene (root);
+
+           primaryStage.setTitle ( "test-Window" );
+           primaryStage.setScene (scene);
+           primaryStage.show ();
+       } catch (Exception e) {
+           e.printStackTrace ();
+       }
+   }
+
+   public  static  void main (String [] args) {
+       launch (args);
+   }
 }
---
+++ Project3/src/TestUI.java
@@ -0,0 +1,27 @@
+package Project3.src;
+
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.FileChooser;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TestUI {
+
+    @FXML
+    private Label bigLabel;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    void onClick(ActionEvent event) {
+      System.out.println("yo yo yo");
+    }
+
+}
