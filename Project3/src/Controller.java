b1c19b5da9d82077a98516459ffaea725f437efd
Vedant <vm.WorkAddress@gmail.com>
2021-03-11 14:05:32 -0500

    added tabPane

--- Project3/src/Controller.java
+++ Project3/src/Controller.java
@@ -7,83 +7,91 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TabPane;
 
 public class Controller {
 
-    @FXML
-    private Tab addTab;
+  @FXML
+  private TabPane mangeEmployees_TabPane;
 
-    @FXML
-    private TextField lastName_Input;
+  @FXML
+  private Tab addTab;
 
-    @FXML
-    private TextField firstName_Input;
+  @FXML
+  private TextField lastName_Input;
 
-    @FXML
-    private RadioButton departmentSelect_CS;
+  @FXML
+  private TextField firstName_Input;
 
-    @FXML
-    private RadioButton departmentSelect_ECE;
+  @FXML
+  private RadioButton departmentSelect_CS_add;
 
-    @FXML
-    private RadioButton departmentSelect_IT;
+  @FXML
+  private RadioButton departmentSelect_ECE_add;
 
-    @FXML
-    private DatePicker dateHired_Input;
+  @FXML
+  private RadioButton departmentSelect_IT_add;
 
-    @FXML
-    private RadioButton employeeTypeSelect_MNGR;
+  @FXML
+  private DatePicker dateHired_Input;
 
-    @FXML
-    private RadioButton employeeTypeSelect_FT;
+  @FXML
+  private RadioButton employeeTypeSelect_MNGR;
 
-    @FXML
-    private RadioButton employeeTypeSelect_PT;
+  @FXML
+  private RadioButton employeeTypeSelect_FT;
 
-    @FXML
-    private TextField hourlyRate_Input;
+  @FXML
+  private RadioButton employeeTypeSelect_PT;
 
-    @FXML
-    private Tab removeTab;
+  @FXML
+  private TextField hourlyRate_Input;
 
-    @FXML
-    private Tab setHoursTab;
+  @FXML
+  private Tab removeTab;
 
-    @FXML
-    private TextField numHours_Input;
+  @FXML
+  private RadioButton departmentSelect_CS;
 
-    @FXML
-    private Button submit;
+  @FXML
+  private RadioButton departmentSelect_ECE;
 
-    @FXML
-    private Button processPayment;
+  @FXML
+  private RadioButton departmentSelect_IT;
 
-    @FXML
-    private Button viewAll_Button;
+  @FXML
+  private Tab setHoursTab;
 
-    @FXML
-    private Button viewDateHired_Button;
+  @FXML
+  private TextField numHours_Input;
 
-    @FXML
-    private Button viewDepartment_Button;
+  @FXML
+  private Button submit;
 
-    @FXML
-    private Button uploadFile_Button;
+  @FXML
+  private Button processPayment;
 
-    @FXML
-    private Button submitImport;
+  @FXML
+  private Button viewAll_Button;
 
-    @FXML
-    private Button export_Button;
+  @FXML
+  private Button viewDateHired_Button;
 
-    @FXML
-    void onSubmit(ActionEvent event) {
-      System.out.println("Yo");
-      if(departmentSelect_CS.isPressed()){
-         String toogleGroupValue = departmentSelect_CS.getText();
-         System.out.println(toogleGroupValue + "was selected");
-      }
-      else System.out.println("nothing selected");
-    }
+  @FXML
+  private Button viewDepartment_Button;
+
+  @FXML
+  private Button uploadFile_Button;
+
+  @FXML
+  private Button submitImport;
+
+  @FXML
+  private Button export_Button;
+
+  @FXML
+  void onSubmit(ActionEvent event) {
+
+  }
 
 }
