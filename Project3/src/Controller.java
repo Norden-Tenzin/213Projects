b43437dedfe0a71ca4b40678cb5edecf374584cd
Vedant <vm.WorkAddress@gmail.com>
2021-03-12 14:42:33 -0500

    added error handling

--- Project3/src/Controller.java
+++ Project3/src/Controller.java
@@ -5,6 +5,7 @@ import javax.swing.JRadioButton;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.annotation.Target;
+import java.lang.reflect.InvocationTargetException;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import javafx.event.ActionEvent;
@@ -200,32 +201,31 @@ public class Controller {
   @FXML
   private Button quitButton;
 
-
-
   @FXML
   void onQuit(ActionEvent event) {
-
+    Stage stage = (Stage) quitButton.getScene().getWindow();
+    stage.close();
   }
 
   /**
-    * TODO
-    *
-    * @param employee employee Object
-    * @return index of the employee if employee is found in the employees array.
-    *         otherwise -1.
-    */
+   * TODO
+   *
+   * @param employee employee Object
+   * @return index of the employee if employee is found in the employees array.
+   *         otherwise -1.
+   */
   @FXML
   void disableManagerTypeSelect(ActionEvent event) {
     managerTypeButtonBar.setDisable(true);
   }
 
   /**
-    * TODO
-    *
-    * @param employee employee Object
-    * @return index of the employee if employee is found in the employees array.
-    *         otherwise -1.
-    */
+   * TODO
+   *
+   * @param employee employee Object
+   * @return index of the employee if employee is found in the employees array.
+   *         otherwise -1.
+   */
   @FXML
   void disableDepartmentSelect(ActionEvent event) {
 
@@ -244,12 +244,12 @@ public class Controller {
   }
 
   /**
-    * TODO
-    *
-    * @param employee employee Object
-    * @return index of the employee if employee is found in the employees array.
-    *         otherwise -1.
-    */
+   * TODO
+   *
+   * @param employee employee Object
+   * @return index of the employee if employee is found in the employees array.
+   *         otherwise -1.
+   */
   @FXML
   void disableEmployeeSelect(ActionEvent event) {
     if (employeeTypeSelect_MNGR_add.isSelected()) {
@@ -266,33 +266,34 @@ public class Controller {
     }
 
   }
-  
+
   /**
-    * 
-    * @param event
-    */
+   * 
+   * @param event
+   */
   @FXML
   void onExportSubmit(ActionEvent event) {
-    
+
     exportOutput.clear();
     FileChooser chooser = new FileChooser();
-		chooser.setTitle("Open Target File for the Export");
-		chooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
-				new ExtensionFilter("All Files", "*.*"));
-		Stage stage = new Stage();
-		File targetFile = chooser.showSaveDialog(stage); 
+    chooser.setTitle("Open Target File for the Export");
+    chooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
+        new ExtensionFilter("All Files", "*.*"));
+    Stage stage = new Stage();
+    File targetFile = chooser.showSaveDialog(stage);
     try {
       pp.exportToFile(targetFile.getName());
       exportOutput.setText("Database exported to " + targetFile.getName());
     } catch (FileNotFoundException e) {
       exportOutput.setText("Unable to export the file - File not found");
-    }//get the reference of the target file
-		//write code to write to the file.
+    } // get the reference of the target file
+      // write code to write to the file.
   }
 
-
   /**
-   * Function that gets triggered when the "Submit" button is pressed on the import page. This calls the helper class to run an importFile function. 
+   * Function that gets triggered when the "Submit" button is pressed on the
+   * import page. This calls the helper class to run an importFile function.
+   * 
    * @param event
    */
   @FXML
@@ -300,31 +301,33 @@ public class Controller {
     importOutput.setText(pp.importFile(uploadedFile));
   }
 
-
-/**
- * Function that gets triggered when the "Upload File" is pressed. This only accepts .txt files.
- * @param event
- */
+  /**
+   * Function that gets triggered when the "Upload File" is pressed. This only
+   * accepts .txt files.
+   * 
+   * @param event
+   */
   @FXML
   void onFileUpload(ActionEvent event) {
     FileChooser chooser = new FileChooser();
-		chooser.setTitle("Open Source File for the Import");
-		chooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
-				new ExtensionFilter("All Files", "*.*"));
-		Stage stage = new Stage();
-		File sourceFile = chooser.showOpenDialog(stage);
+    chooser.setTitle("Open Source File for the Import");
+    chooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
+        new ExtensionFilter("All Files", "*.*"));
+    Stage stage = new Stage();
+    File sourceFile = chooser.showOpenDialog(stage);
     uploadFile_Button.setText(sourceFile.getName());
-    if(sourceFile.exists()){
+    if (sourceFile.exists()) {
       submitImport.setDisable(false);
       uploadedFile = sourceFile;
     }
-    
+
   }
 
   /**
-    * Function for Button ProcessPayment which processes the payment when clicked. 
-    * @param event
-    */
+   * Function for Button ProcessPayment which processes the payment when clicked.
+   * 
+   * @param event
+   */
   @FXML
   void onProcessPayment(ActionEvent event) {
     String input = "C";
@@ -332,12 +335,14 @@ public class Controller {
   }
 
   /**
-    * Function for Button Submit which creates the input command depending on the inputs provided by the user.
-    * This function handles 
-    * @param event
-    */
+   * Function for Button Submit which creates the input command depending on the
+   * inputs provided by the user. This function handles
+   * 
+   * @param event
+   */
   @FXML
-  void onSubmit(ActionEvent event) {
+  void onSubmit(ActionEvent event) throws InvocationTargetException {
+
     String finalOutput = "";
 
     String input = "";
@@ -348,80 +353,83 @@ public class Controller {
     String lastName = "";
     String dateHired = "";
     String hourlyRate = "";
-    String numHours = ""; 
+    String numHours = "";
+    try {
+      int tabIndex = mangeEmployees_TabPane.getSelectionModel().getSelectedIndex();
+      switch (tabIndex) {
+      case 0:
+        firstName = firstName_Input_add.getText();
+        lastName = lastName_Input_add.getText();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/YYYY");
+        dateHired = formatter.format(dateHired_Input_add.getValue());
+        hourlyRate = hourlyRate_Input.getText();
 
-    int tabIndex = mangeEmployees_TabPane.getSelectionModel().getSelectedIndex();
-    switch (tabIndex) {
-    case 0:
-      firstName = firstName_Input_add.getText();
-      lastName = lastName_Input_add.getText();
-      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/YYYY"); 
-      dateHired = formatter.format(dateHired_Input_add.getValue());
-      hourlyRate = hourlyRate_Input.getText();
+        if (departmentSelect_CS_add.isSelected())
+          department = "CS";
+        else if (departmentSelect_ECE_add.isSelected())
+          department = "ECE";
+        else if (departmentSelect_IT_add.isSelected())
+          department = "IT";
 
-      if (departmentSelect_CS_add.isSelected())
-        department = "CS";
-      else if (departmentSelect_ECE_add.isSelected())
-        department = "ECE";
-      else if (departmentSelect_IT_add.isSelected())
-        department = "IT";
+        if (employeeTypeSelect_MNGR_add.isSelected())
+          command = "M";
+        else if (employeeTypeSelect_FT_add.isSelected())
+          command = "F";
+        else if (employeeTypeSelect_PT_add.isSelected())
+          command = "P";
 
-      if (employeeTypeSelect_MNGR_add.isSelected())
-        command = "M";
-      else if (employeeTypeSelect_FT_add.isSelected())
-        command = "F";
-      else if (employeeTypeSelect_PT_add.isSelected())
-        command = "P";
+        if (managerTypeSelect_MNGR.isSelected())
+          managerType = "1";
+        else if (managerTypeSelect_DH.isSelected())
+          managerType = "2";
+        else if (managerTypeSelect_Dir.isSelected())
+          managerType = "3";
 
-      if (managerTypeSelect_MNGR.isSelected())
-        managerType = "1";
-      else if (managerTypeSelect_DH.isSelected())
-        managerType = "2";
-      else if (managerTypeSelect_Dir.isSelected())
-        managerType = "3";
+        input = command + " " + lastName + "," + firstName + " " + department + " " + dateHired + " " + hourlyRate + " "
+            + managerType;
+        break;
 
-      input = command + " " + lastName + "," + firstName + " " + department + " " + dateHired + " " + hourlyRate + " "
-          + managerType;
-      break;
+      case 1:
+        firstName = firstName_Input_rmv.getText();
+        lastName = lastName_Input_rmv.getText();
+        dateHired = dateHired_Input_rmv.getValue().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
 
-    case 1:
-      firstName = firstName_Input_rmv.getText();
-      lastName = lastName_Input_rmv.getText();
-      dateHired = dateHired_Input_rmv.getValue().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
+        if (departmentSelect_CS_rmv.isSelected())
+          department = "CS";
+        else if (departmentSelect_ECE_rmv.isSelected())
+          department = "ECE";
+        else if (departmentSelect_IT_rmv.isSelected())
+          department = "IT";
 
-      if (departmentSelect_CS_rmv.isSelected())
-        department = "CS";
-      else if (departmentSelect_ECE_rmv.isSelected())
-        department = "ECE";
-      else if (departmentSelect_IT_rmv.isSelected())
-        department = "IT";
+        input = "R " + lastName + "," + firstName + " " + department + " " + dateHired;
+        break;
 
-      input = "R " + lastName + "," + firstName + " " + department + " " + dateHired;
-      break;
+      case 2:
+        firstName = firstName_Input_sh.getText();
+        lastName = lastName_Input_sh.getText();
+        dateHired = dateHired_Input_sh.getValue().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
 
-    case 2:
-      firstName = firstName_Input_sh.getText();
-      lastName = lastName_Input_sh.getText();
-      dateHired = dateHired_Input_sh.getValue().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
+        if (departmentSelect_CS_sh.isSelected())
+          department = "CS";
+        else if (departmentSelect_ECE_sh.isSelected())
+          department = "ECE";
+        else if (departmentSelect_IT_sh.isSelected())
+          department = "IT";
 
-      if (departmentSelect_CS_sh.isSelected())
-        department = "CS";
-      else if (departmentSelect_ECE_sh.isSelected())
-        department = "ECE";
-      else if (departmentSelect_IT_sh.isSelected())
-        department = "IT";
+        numHours = numHours_Input.getText();
 
-      numHours = numHours_Input.getText();
-
-      input = "S " + lastName + "," + firstName + " " + department + " " + dateHired + " " + numHours;
-      break;
+        input = "S " + lastName + "," + firstName + " " + department + " " + dateHired + " " + numHours;
+        break;
+      }
+      messageOuput.setText(reverseOutput(pp.run(input)));
+    } catch (Exception e) {
+      messageOuput.setText("Please input all necessary values");
     }
-    messageOuput.setText(reverseOutput(pp.run(input)));
   }
 
   // for printing
   @FXML
-  void onViewAll(ActionEvent event) { 
+  void onViewAll(ActionEvent event) {
     printOutput.clear();
     String input = "PA";
 
@@ -433,7 +441,7 @@ public class Controller {
     printOutput.clear();
 
     String input = "PH";
-    
+
     printOutput.setText(pp.run(input));
   }
 
@@ -441,7 +449,6 @@ public class Controller {
   void onViewDepartment(ActionEvent event) {
     printOutput.clear();
 
-    
     String input = "PD";
 
     printOutput.setText(pp.run(input));
@@ -484,7 +491,6 @@ public class Controller {
     employeeTypeSelect_PT_add.setSelected(false);
     employeeTypeSelect_MNGR_add.setSelected(false);
 
-
     managerTypeSelect_MNGR.setSelected(false);
     managerTypeSelect_DH.setSelected(false);
     managerTypeSelect_Dir.setSelected(false);
@@ -506,14 +512,17 @@ public class Controller {
   }
 
   /**
-   * Reverses the output of the payroll processing run() method for the UI. 
-   * @param str output string of the run() method used by the payment processing class
-   * @return the reversed string of the ouput string generated by the run() method.
+   * Reverses the output of the payroll processing run() method for the UI.
+   * 
+   * @param str output string of the run() method used by the payment processing
+   *            class
+   * @return the reversed string of the ouput string generated by the run()
+   *         method.
    */
-  public String reverseOutput(String str){
+  public String reverseOutput(String str) {
     String outputString = "";
-    String [] splittedString = str.split("\n");
-    for(int i=splittedString.length-1;i>=0;i--){
+    String[] splittedString = str.split("\n");
+    for (int i = splittedString.length - 1; i >= 0; i--) {
       outputString += splittedString[i] + "\n";
     }
     return outputString;
